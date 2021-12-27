e3750d8f82ea9d9a8f3f3de37681379d83117af4
dudgns3443 <williams3443@gmail.com>
2021-12-27 16:32:09 +0900

    session clustering

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const url = `mongodb+srv://hyperlogic:6408@healthcare.s6oc1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
-
-
-
 var mysql      = require('mysql');
 
 var connection = mysql.createConnection({
@@ -125,49 +122,52 @@ MongoClient.connect(url,(e,client)=>{
 
 
 })
-// var session = require('express-session');
-// var RedisStore = require('connect-redis')(session);
+var redis = require('redis');
+(async () => {
+    const url = `redis://cluster-example.hqrloz.0001.apn2.cache.amazonaws.com:6379`;
+    var session = require('express-session');
+    var RedisStore = require('connect-redis')(session);
 
-// var redis = require('redis');
-// var redisClient = redis.createClient(
-//   {
-//     port: 6379, 
-//     host: 'cluster-example.hqrloz.0001.apn2.cache.amazonaws.com'
-//   }
-// );
-// (async () => {
-// redisClient.on('error', function (err) {
-//   console.log('Could not establish a connection with redis. ' + err);
-// });
-// redisClient.on('connect', function (err) {
-//   console.log('Connected to redis successfully');
-// });
-// await redisClient.connect();
-// })();
-// app.use(session({
-//     secret: 'redis-session-test',
-//     store: new RedisStore({client: redisClient}),
-//     resave: false,
-//     saveUninitialized: true
-// }));
+    const redisClient = redis.createClient(
+    {
+        url
+    }
+    );
+    redisClient.on('error', function (err) {
+    console.log('Could not establish a connection with redis. ' + err);
+    });
+    redisClient.on('connect', function (err) {
+    console.log('Connected to redis successfully');
+    });
+    await redisClient.connect();
+    app.use(session({
+        secret: 'redis-session-test',
+        store: new RedisStore({client: redisClient}),
+        resave: false,
+        saveUninitialized: true
+    }));
+    
+    app.get('/session', function (req, res) {
+      try{
+        var session = req.session;
+        console.log(session.user);
+        if (session.user) {
+            res.send({
+              msg :'session already saved. user = ' + session.user,
+              bool : true
+          });
+        } else {
+            session.user = 'test';
+            res.send({
+              msg : 'session saved',
+              bool : false
+            });
+            
+        }
+      } catch (err){ next(err)}
+    });  
+    
 
-// app.get('/session', function (req, res) {
-//   try{
-//     var session = req.session;
-//     console.log(session.user);
-//     if (session.user) {
-//         res.send({
-//           msg :'session already saved. user = ' + session.user,
-//           bool : true
-//       });
-//     } else {
-//         session.user = 'test';
-//         res.send({
-//           msg : 'session saved',
-//           bool : false
-//         });
-        
-//     }
-//   } catch (err){ next(err)}
-// });           
+})();
+
 app.listen(8100)
